0e92973a5751a2453fd5bcf6e275a60aac5b6a33
RaspiAPC <Raspi@apc.de>
2018-06-13 20:26:12 +0200

    init helper.py: handles json for die initial install.sh script

---
+++ project/installer/helper.py
@@ -0,0 +1,14 @@
+import sys, json
+
+print "This is the name of the script: ", sys.argv[0]
+print "Number of arguments: ", len(sys.argv)
+print "The arguments are: " , str(sys.argv)
+
+filename = sys.argv[1]
+url = sys.argv[2]
+id = sys.argv[3]
+im = sys.argv[4]
+ic = sys.argv[5]
+
+fo = open(filename, 'w+')
+fo.write(json.dumps({'id':id,'intervalMeasures':im,'intervalConfig':ic,'serverUrl':url}))
