b65aef823abdb4906de3dbd82e920200a98e456f
Felix <fschilk@fh-bielefeld.de>
2018-06-19 20:49:58 +0200

    update launcher.sh

--- src/launcher.sh
+++ src/launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/.apc
-sudo python main.py
+cd ${HOME}.apc
+python main.py > ${HOME}.apc/log/log
 cd /
