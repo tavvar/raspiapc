da5bc59993f44ccfa9f7c0f78f5f25e0b38f383e
Felix <fschilk@fh-bielefeld.de>
2018-06-19 21:07:17 +0200

    small fix

--- src/launcher.sh
+++ src/launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd ${HOME}.apc
-python main.py > ${HOME}.apc/log/log
+cd ${HOME}/.apc
+python main.py
 cd /
