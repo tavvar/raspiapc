0046160723eba48e123d599c42dc270d36270520
RaspiAPC <Raspi@apc.de>
2018-07-09 14:41:53 +0200

    launcher edited

--- src/launcher.sh
+++ src/launcher.sh
@@ -3,6 +3,9 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 #cd /
-cd ${HOME}/.apc
-stdbuf -oL python main.py & > log
+cd ${HOME}/.apc	
+DATE="`date +%F_%T`.log" 
+mkdir logs
+#touch logs/$DATE
+stdbuf -oL python main.py > logs/$DATE
 #cd /
